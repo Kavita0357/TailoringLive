ebabf1388e2b59dfbcaacfcb4509c8ed0675844d
Kavita Bavadiya <kavitabavadiya@gmail.com>
2026-05-25 22:19:06 +0530

    Update business setting design

--- resources/views/layouts/partials/search_settings.blade.php
+++ resources/views/layouts/partials/search_settings.blade.php
@@ -1,10 +1,18 @@
+<style type="text/css">
+    .search-input {
+        border-bottom: 1px solid #d2d6de;
+        padding-bottom: 20px;
+    }
+</style>
 <div class="row">
-    <div class="col-md-8 col-xs-12 col-md-offset-2">
-        <div class="input-group">
+    <div class="col-md-12 col-xs-12" style="padding-left: 33px;">
+        <div class="setting-title">
+            <h3 class="tw-font-bold">@lang('business.business_settings')</h3>
+        </div>
+        <div class="search-input input-group">
             <span class="input-group-addon"><i class="fa fa-search"></i></span>
             <select id="search_settings" class="form-control" style="width: 100%;">
             </select>
         </div>
-        
     </div>
 </div>
