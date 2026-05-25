141d532ba0de384b3f7f3b0e73050a8750fa7c89
Kavita Bavadiya <kavitabavadiya@gmail.com>
2026-05-25 23:14:42 +0530

    Remove borders from business setting

--- resources/views/layouts/partials/search_settings.blade.php
+++ resources/views/layouts/partials/search_settings.blade.php
@@ -1,18 +1,22 @@
 <style type="text/css">
     .search-input {
-        border-bottom: 1px solid #d2d6de;
+        padding-top: 10px;
         padding-bottom: 20px;
     }
 </style>
 <div class="row">
     <div class="col-md-12 col-xs-12" style="padding-left: 33px;">
-        <div class="setting-title">
-            <h3 class="tw-font-bold">@lang('business.business_settings')</h3>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 pos-tab-menu tw-rounded-lg">
+            <div class="setting-title">
+                <h3 class="tw-font-bold">@lang('business.business_settings')</h3>
+            </div>
         </div>
-        <div class="search-input input-group">
-            <span class="input-group-addon"><i class="fa fa-search"></i></span>
-            <select id="search_settings" class="form-control" style="width: 100%;">
-            </select>
+        <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
+            <div class="search-input input-group">
+                <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                <select id="search_settings" class="form-control" style="width: 100%;">
+                </select>
+            </div>
         </div>
     </div>
 </div>
