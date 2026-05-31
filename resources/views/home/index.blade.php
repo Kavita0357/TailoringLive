48e7ba74ba2bfd4fd013c33f85f0d3553e1ed354
kaushalgorasiya <kaushalgorasiya@gmail.com>
2026-05-31 21:26:50 +0530

    orders payment due and deliver Table  1 row add

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -658,8 +658,9 @@
                         </div>
                     @endif
                 @endif
+                <div class="lg:tw-col-span-2 tw-flex tw-flex-col lg:tw-flex-row tw-gap-4 sm:tw-gap-5 tw-w-full">
                 <div
-                    class="tw-transition-all lg:tw-col-span-2 tw-duration-200 tw-bg-white tw-shadow-sm tw-rounded-xl tw-ring-1 hover:tw-shadow-md hover:tw--translate-y-0.5 tw-ring-gray-200">
+                    class="tw-flex-1 tw-min-w-0 tw-transition-all tw-duration-200 tw-bg-white tw-shadow-sm tw-rounded-xl tw-ring-1 hover:tw-shadow-md hover:tw--translate-y-0.5 tw-ring-gray-200">
                     <div class="tw-p-4 sm:tw-p-5">
                         <div class="tw-flex tw-items-center tw-gap-2.5">
                             <div
@@ -713,7 +714,7 @@
                     </div>
                 </div>
                 <div
-                    class="tw-transition-all lg:tw-col-span-2 tw-duration-200 tw-bg-white tw-shadow-sm tw-rounded-xl tw-ring-1 hover:tw-shadow-md hover:tw--translate-y-0.5 tw-ring-gray-200">
+                    class="tw-flex-1 tw-min-w-0 tw-transition-all tw-duration-200 tw-bg-white tw-shadow-sm tw-rounded-xl tw-ring-1 hover:tw-shadow-md hover:tw--translate-y-0.5 tw-ring-gray-200">
                     <div class="tw-p-4 sm:tw-p-5">
                         <div class="tw-flex tw-items-center tw-gap-2.5">
                             <div
@@ -768,6 +769,7 @@
                             </div>
                         </div>
                     </div>
+                </div>
                 </div>
             </div>
         </div>
