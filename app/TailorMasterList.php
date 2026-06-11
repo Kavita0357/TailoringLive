d115cd74d71a41079b5a6ab6de18f5e6225e4f12
vidz0509 <vairagbavadiya96@gmail.com>
2026-06-11 22:50:07 +0530

    Solve issue

--- app/TailorMasterList.php
+++ app/TailorMasterList.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TailorMasterList extends Model
 {
@@ -11,4 +12,9 @@ class TailorMasterList extends Model
     protected $guarded = ['id'];
 
     public $timestamps = true;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
