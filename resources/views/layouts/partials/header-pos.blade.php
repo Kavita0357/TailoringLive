2840eee3c3e5249a4720d12d54dceee897e1b29b
Kavita Bavadiya <kavitabavadiya@gmail.com>
2026-08-27 12:32:56 +0530

    Resolve issues shared on Whatsapp

--- resources/views/layouts/partials/header-pos.blade.php
+++ resources/views/layouts/partials/header-pos.blade.php
@@ -149,139 +149,6 @@
 <div class="col-md-12 no-print pos-header">
     <input type="hidden" id="pos_redirect_url" value="{{ $pos_redirect_url }}">
 
-    {{-- <div class="row">
-      
-        <div class="col-md-6">
-            <div class="m-6 mt-5" style="display: flex;">
-                <p><strong>@lang('sale.location'): &nbsp;</strong>
-                    @if (empty($transaction->location_id))
-                        @if (count($business_locations) > 1)
-                            <div style="width: 28%;margin-bottom: 5px;">
-                                {!! Form::select(
-                                    'select_location_id',
-                                    $business_locations,
-                                    $default_location->id ?? null,
-                                    ['class' => 'form-control input-sm', 'id' => 'select_location_id', 'required', 'autofocus'],
-                                    $bl_attributes,
-                                ) !!}
-                            </div>
-                        @else
-                            {{ $default_location->name }}
-                        @endif
-                    @endif
-
-                    @if (!empty($transaction->location_id))
-                        {{ $transaction->location->name }}
-                    @endif &nbsp; <span class="curr_datetime">{{ @format_datetime('now') }}</span>
-                    <i class="fa fa-keyboard hover-q text-muted" aria-hidden="true" data-container="body"
-                        data-toggle="popover" data-placement="bottom" data-content="@include('sale_pos.partials.keyboard_shortcuts_details')"
-                        data-html="true" data-trigger="hover" data-original-title="" title=""></i>
-                </p>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <a href="{{ $go_back_url }}" title="{{ __('lang_v1.go_back') }}"
-                class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-info m-5 pull-right">
-                <strong><i class="fa fa-backward fa-lg"></i></strong>
-            </a>
-            @if (!empty($pos_settings['inline_service_staff']))
-                <button type="button" id="show_service_staff_availability"
-                    title="{{ __('lang_v1.service_staff_availability') }}"
-                    class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-primary btn-flat m-6 btn-xs m-5 pull-right"
-                    data-container=".view_modal"
-                    data-href="{{ action([\App\Http\Controllers\SellPosController::class, 'showServiceStaffAvailibility']) }}">
-                    <strong><i class="fa fa-users fa-lg"></i></strong>
-                </button>
-            @endif
-
-            @can('close_cash_register')
-                <button type="button" id="close_register" title="{{ __('cash_register.close_register') }}"
-                    class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-error m-5 btn-modal pull-right"
-                    data-container=".close_register_modal"
-                    data-href="{{ action([\App\Http\Controllers\CashRegisterController::class, 'getCloseRegister']) }}">
-                    <strong><i class="fa fa-window-close fa-lg"></i></strong>
-                </button>
-            @endcan
-
-            @if (!empty($pos_settings['inline_service_staff']) || (in_array('tables', $enabled_modules) || in_array('service_staff', $enabled_modules)))
-                <button type="button"
-                    class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-primary m-6 btn-xs m-5 pull-right popover-default"
-                    id="service_staff_replacement" title="{{ __('restaurant.service_staff_replacement') }}"
-                    data-toggle="popover" data-trigger="click"
-                    data-content='<div class="m-8"><input type="text" class="form-control" placeholder="@lang('sale.invoice_no')" id="send_for_sell_service_staff_invoice_no"></div><div class="w-100 text-center"><button type="button" class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-error" id="send_for_sercice_staff_replacement">@lang('lang_v1.send')</button></div>'
-                    data-html="true" data-placement="bottom">
-                    <strong><i class="fa fa-user-plus fa-lg"></i></strong>
-                </button>
-            @endif
-
-            @can('view_cash_register')
-                <button type="button" id="register_details" title="{{ __('cash_register.register_details') }}"
-                    class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-success m-6 btn-xs m-5 btn-modal pull-right"
-                    data-container=".register_details_modal"
-                    data-href="{{ action([\App\Http\Controllers\CashRegisterController::class, 'getRegisterDetails']) }}">
-                    <strong><i class="fa fa-briefcase fa-lg" aria-hidden="true"></i></strong>
-                </button>
-            @endcan
-
-            <button title="@lang('lang_v1.calculator')" id="btnCalculator" type="button"
-                class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-success pull-right m-5 btn-xs mt-10 popover-default"
-                data-toggle="popover" data-trigger="click" data-content='@include('layouts.partials.calculator')' data-html="true"
-                data-placement="bottom">
-                <strong><i class="fa fa-calculator fa-lg" aria-hidden="true"></i></strong>
-            </button>
-
-            <button type="button"
-                class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-error m-6 m-5 pull-right popover-default"
-                id="return_sale" title="@lang('lang_v1.sell_return')" data-toggle="popover" data-trigger="click"
-                data-content='<div class="m-8"><input type="text" class="form-control" placeholder="@lang('sale.invoice_no')" id="send_for_sell_return_invoice_no"></div><div class="w-100 text-center"><button type="button" class="tw-dw-btn tw-dw-btn-error tw-text-white tw-dw-btn-sm" id="send_for_sell_return">@lang('lang_v1.send')</button></div>'
-                data-html="true" data-placement="bottom">
-                <strong><i class="fas fa-undo fa-lg"></i></strong>
-            </button>
-
-            <button type="button" title="{{ __('lang_v1.full_screen') }}"
-                class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-primary m-6 hidden-xs m-5 pull-right"
-                id="full_screen">
-                <strong><i class="fa fa-window-maximize fa-lg"></i></strong>
-            </button>
-
-            <button type="button" id="view_suspended_sales" title="{{ __('lang_v1.view_suspended_sales') }}"
-                class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-yellow m-6 btn-xs m-5 btn-modal pull-right"
-                data-container=".view_modal" data-href="{{ $view_suspended_sell_url }}">
-                <strong><i class="fa fa-pause-circle fa-lg"></i></strong>
-            </button>
-            @if (empty($pos_settings['hide_product_suggestion']))
-                <button type="button" title="{{ __('lang_v1.view_products') }}" data-placement="bottom"
-                    class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-success m-6 btn-xs m-5 btn-modal pull-right tw-block md:tw-hidden"
-                    data-toggle="modal" data-target="#mobile_product_suggestion_modal">
-                    <strong><i class="fa fa-cubes fa-lg"></i></strong>
-                </button>
-            @endif
-
-            @if (Module::has('Repair') && $transaction_sub_type != 'repair')
-                @include('repair::layouts.partials.pos_header')
-            @endif
-
-            @if (in_array('pos_sale', $enabled_modules) && !empty($transaction_sub_type))
-                @can('sell.create')
-                    <a href="{{ action([\App\Http\Controllers\SellPosController::class, 'create']) }}"
-                        title="@lang('sale.pos_sale')"
-                        class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-success m-6 btn-xs m-5 pull-right">
-                        <strong><i class="fa fa-th-large"></i> &nbsp; @lang('sale.pos_sale')</strong>
-                    </a>
-                @endcan
-            @endif
-            @can('expense.add')
-                <button type="button" title="{{ __('expense.add_expense') }}" data-placement="bottom"
-                    class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-primary m-6 btn-xs m-5 btn-modal pull-right"
-                    id="add_expense">
-                    <strong><i class="fa fas fa-minus-circle"></i> @lang('expense.add_expense')</strong>
-                </button>
-            @endcan
-
-        </div>
-
-    </div> --}}
-
     <div class="tw-flex tw-flex-col md:tw-flex-row tw-items-center tw-justify-between tw-shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] tw-mx-0 tw-mt-1 tw-mb-0 md:tw-mb-0 tw-w-full"
         style="padding: 15px;">
         <div class="location">
@@ -338,6 +205,16 @@
             </div>
         </div>
         @if (request()->segment(1) == 'cloth-pos' && !empty($pos_settings['enable_transaction_date']))
+            @php
+                $business_date_format = session('business.date_format', config('constants.default_date_format'));
+
+                $business_time_format = session('business.time_format');
+
+                $carbon_time_format = $business_time_format == 12 ? 'h:i A' : 'H:i';
+
+                $datetime_format = $business_date_format . ' ' . $carbon_time_format;
+            @endphp
+
             <div class="order-dates">
                 <div class="order-date-group">
                     <p class="tw-text-white"><strong>@lang('tailoring.order_date'):</strong></p>
@@ -358,8 +235,10 @@
                         {!! Form::text(
                             'transaction_date',
                             !empty($transaction->transaction_date)
-                                ? \Carbon\Carbon::parse($transaction->transaction_date)->format('m/d/Y h:i A')
-                                : $default_datetime ?? now()->format('m/d/Y h:i A'),
+                                ? \Carbon\Carbon::parse($transaction->transaction_date)->format($datetime_format)
+                                : (!empty($default_datetime)
+                                    ? \Carbon\Carbon::parse($default_datetime)->format($datetime_format)
+                                    : now()->format($datetime_format)),
                             $order_date_attr,
                         ) !!}
                     </div>
@@ -373,8 +252,10 @@
                         {!! Form::text(
                             'delivery_date',
                             !empty($transaction->delivery_date)
-                                ? \Carbon\Carbon::parse($transaction->delivery_date)->format('m/d/Y h:i A')
-                                : $default_datetime ?? now()->format('m/d/Y h:i A'),
+                                ? \Carbon\Carbon::parse($transaction->delivery_date)->format($datetime_format)
+                                : (!empty($default_datetime)
+                                    ? \Carbon\Carbon::parse($default_datetime)->format($datetime_format)
+                                    : now()->format($datetime_format)),
                             [
                                 'class' => 'form-control text-white tw-font-semibold',
                                 'id' => 'delivery_date',
@@ -548,8 +429,7 @@
                     <button type="button" title="{{ __('expense.add_expense') }}" data-placement="bottom"
                         id="add_expense"
                         class="btn-modal tw-inline-flex tw-items-center tw-gap-2 tw-bg-white tw-border tw-border-gray-300 tw-rounded-md tw-px-4 tw-py-2 tw-whitespace-nowrap hover:tw-bg-gray-50 tw-shadow-sm"
-                        data-href="{{ action([\App\Http\Controllers\ExpenseController::class, 'create']) }}?modal=true"
-                        data-container=".view_modal">
+                        data-href="{{ action([\App\Http\Controllers\ExpenseController::class, 'create']) }}?modal=true">
 
                         <i class="fas fa-minus tw-text-red-500"></i>
                         <span class="tw-font-semibold tw-whitespace-nowrap">@lang('expense.add_expense')</span>
